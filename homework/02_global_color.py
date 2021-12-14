da70ad2b091945fc94c080ab9b8caf6f0133e4b4
Alexander Davydov <vetrovaikis@gmail.com>
2021-12-14 14:30:29 +0300

    done colors

--- homework/02_global_color.py
+++ homework/02_global_color.py
@@ -16,4 +16,40 @@ import simple_draw as sd
 
 # TODO здесь ваш код
 
+def polygon(point, angle_number, color, angle=0, length=200, width=3):
+    angles_sum = (angle_number - 2) * 180
+    internal_angle = angles_sum / angle_number
+    external_angle = 180 - internal_angle
+    add_angle = 0
+    while add_angle<360:
+        vect = sd.get_vector(start_point=point, angle=angle+add_angle, length=length, width=width)
+        point = vect.end_point
+        add_angle+=external_angle
+        vect.draw(color=color)
+
+color_list = ('COLOR_RED', 'COLOR_ORANGE', 'COLOR_YELLOW', 'COLOR_GREEN', 'COLOR_CYAN', 'COLOR_BLUE', 'COLOR_PURPLE')
+color_dict = {'COLOR_RED':sd.COLOR_RED, 'COLOR_ORANGE':sd.COLOR_ORANGE, 'COLOR_YELLOW':sd.COLOR_YELLOW, 
+              'COLOR_GREEN':sd.COLOR_GREEN, 'COLOR_CYAN':sd.COLOR_CYAN, 'COLOR_BLUE':sd.COLOR_BLUE, 
+              'COLOR_PURPLE':sd.COLOR_PURPLE}
+print('Дотупные цвета:')
+for index, value in enumerate(color_list):
+    print(index, ' : ', value)
+
+angle_number = input('Please, choose the number of angles: ')
+angle = input('Please, choose the incline angle: ')
+length = input('Please, choose the face length: ')
+width = input('Please, choose the face width: ')
+color = input('Please, chooose color index: ')
+
+angle_number = int(angle_number)
+angle = int(angle)
+length = int(length)
+width = int(width)
+color = color_list[int(color)]
+
+color = color_dict[color]
+
+point = sd.get_point(300,300)
+polygon(point=point, angle_number=angle_number, angle=angle, length=length, width=width, color=color)
+
 sd.pause()
